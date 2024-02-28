b0ba9f0fd47289ca1a29730aa3c89b7681f42dc0
PriyankaPK26 <118288268+PriyankaPK26@users.noreply.github.com>
2024-02-28 18:15:22 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function shortcut(s1, s2) {
-     if (!s1 || !s2) {
+    function shortcut(s1, s2) {
+    if (!s1 || !s2) {
         return '';
     }
 
     const initialLetters = s1.charAt(0).toUpperCase() + s2.charAt(0).toLowerCase();
     return initialLetters;
+}
 }
 
 // Do not change the code below.
