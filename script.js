24bf3476dd808553332cfad11a8cadc99af6f7d2
PriyankaPK26 <118288268+PriyankaPK26@users.noreply.github.com>
2024-02-28 17:53:19 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function shortcut(s1, s2) {
-  // your code here
+    if (s1 && s2) {
+        return s1[0].toUpperCase() + s2[0].toLowerCase();
+    } else {
+        return '';
+    }
 }
 
 // Do not change the code below.
